ee4c8f5d6655b34e021b416616b349dd69e00746
nabendu82 <nabendu.biswas@gmail.com>
2022-07-20 20:44:20 +0530

    Hooks 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
-import ListPost from './components/ListPost';
-// import ParentComponent from './components/ParentComponent';
-// import UserGreeting from './components/UserGreeting';
-// import Counter from './components/Counter';
-// import { Greet, ReactGreet } from './components/Greet';
-// import Greeting from './components/Greeting';
-// import Welcome from './components/Welcome';
+import PostForm from './components/PostForm';
+// import GrandParent from './components/GrandParent';
+// import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <ReactGreet /> */}
-      {/* <Greeting name="JavaScript">Language of the web</Greeting>
-      <Greeting name="Golang" />
-      <Greeting name="Swift">Language for ios apps</Greeting>
-      <hr />
-      <Welcome name="Python">Language of the web</Welcome>
-      <Welcome name="flutter" />
-      <Welcome name="Java"><button>Click for Java</button></Welcome> */}
-      {/* <Counter /> */}
-      {/* <ParentComponent /> */}
-      {/* <UserGreeting /> */}
-      <ListPost />
+      {/* <UserProvider value="Nabendu">
+        <GrandParent />
+      </UserProvider> */}
+      <PostForm />
     </div>
   );
 }
